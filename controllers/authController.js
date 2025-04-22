dea88eeb980f9cb267d80999a25e92df16943706
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2025-04-22 15:40:18 +0500

    Fixed invoice issues

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,13 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const asyncErrorHandler = require("../utils/asyncErrorHandler");
 const CustomError = require("../utils/CustomError");
+const Subscription = require("../models/Subscription");
 
 exports.register = asyncErrorHandler(async (request, response, next) => {
     const { name, email, password, role } = request.body;
 
     const existing = await User.findOne({ where: { email } });
-    if (existing){
+    if (existing) {
         const error = new CustomError("Email already registered", 400);
         return next(error);
     }
@@ -23,17 +24,29 @@ exports.register = asyncErrorHandler(async (request, response, next) => {
 exports.login = asyncErrorHandler(async (request, response, next) => {
     const { email, password } = request.body;
 
-    const user = await User.findOne({ where: { email } });
-    if (!user){
+    let user = await User.findOne({
+        where: { email },
+        attributes: ["accountNumber", "name", "email", "role", "password", "isSubscribed", "apiKey", "customerId", "stripeSubId"],
+        include: {
+            model: Subscription,
+            as: "Subscription",
+            attributes: ["subscriptionId", "name", "price", "description", "transactionLimit", "transactionPerDay", "invoice", "priority"], 
+        },
+    });
+
+    if (!user) {
         const error = new CustomError("Invalid credentials", 400);
         return next(error);
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch){
+    if (!isMatch) {
         const error = new CustomError("Invalid credentials", 400);
         return next(error);
     }
+
+    user = user.toJSON();
+    delete user.password;
 
     const token = jwt.sign(
         {
---
+++ models/StripeInvoice.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+const User = require("./User");
+
+const StripeInvoice = sequelize.define("StripeInvoice", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    userId: {
+        type: DataTypes.UUID,
+        references: {
+            model: User,
+            key: "accountNumber",
+        },
+        invoiceUrl: {
+            type: DataTypes.UUID,
+        },
+    },
+});
+
+module.exports = StripeInvoice;
